37757152deee7affcc79614c0b936471452d981f
dustytrash <Lionelbergen@live.com>
2021-05-18 18:16:21 -0500

    added validation to variants

--- src/main/kotlin/dad/business/admin/ManageProductsController.kt
+++ src/main/kotlin/dad/business/admin/ManageProductsController.kt
@@ -21,6 +21,8 @@ class ManageProductsController {
         @RequestParam(value = "sortBy", required = false) sortByValue: Int?,
         @RequestParam(value = "productName", required = false) productName: String?,
         @RequestParam(value = "productCategory", required = false) productCategories: List<Int>?,
+        @RequestParam(value = "variantName", required = false) variantNames: List<String>?,
+        @RequestParam(value = "variantPrice", required = false) variantPrices: List<Double>?,
         request: HttpServletRequest, model: Model): String {
         val session: HttpSession = request.getSession(true)
         var currentLoggedInUser: User? = session.getAttribute("user") as User?
@@ -30,21 +32,34 @@ class ManageProductsController {
         } else if (!currentLoggedInUser.userTypes.contains(UserType.ADMIN)) {
             return "redirect:/store";
         }
+        
+        model.addAttribute("categories", DATBASE_UTIL.getAllCategories())
 
         if (!categoryName.isNullOrBlank() && sortByValue != null) {
             DATBASE_UTIL.insertCategory(categoryName, parentCategoryId, sortByValue)
 
             model.addAttribute("feedback", "Added Category!")
             return "redirect:manageProducts";
-        } else if (!productName.isNullOrBlank() && !productCategories!!.isEmpty()){
+        } else if (!productName.isNullOrBlank() && productCategories!!.isNotEmpty()){
             println(productName)
             println(productCategories)
 
-            DATBASE_UTIL.insertProduct(productName, productCategories)
+            if (!variantNames.isNullOrEmpty() && !variantPrices.isNullOrEmpty()) {
+                val filteredVariantNames = variantNames.filter { e -> !e.isNullOrEmpty() };
+                val filteredVariantPrices = variantPrices.filter { e -> e != null && !e.isNaN() };
+                if (filteredVariantNames.size != filteredVariantPrices.size) {
+                    model.addAttribute("feedback", "Invalid Variant!")
+                    return "admin/manageProducts";
+                }
+
+                println(variantNames)
+                println(variantPrices)
+            }
+
+            // DATBASE_UTIL.insertProduct(productName, productCategories)
             model.addAttribute("feedback", "Added Product!")
         }
 
-        model.addAttribute("categories", DATBASE_UTIL.getAllCategories())
         return "admin/manageProducts";
     }
 }
